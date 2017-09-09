24d23fa5485589b9649f81335a8eb50054e781ee
veronicanisenbaum <veroyv412@gmail.com>
2017-09-09 18:26:00 -0300

    fix text

--- site/partials/novedades.php
+++ site/partials/novedades.php
@@ -3,13 +3,42 @@
         <h1>Novedades</h1>
     </div>
     <br>
+
+    <div class="row" id="next-novedad" data-animate="fadeInRight">
+        <div id="posts" class="events small-thumbs">
+            <div class="entry clearfix" data-animate="fadeIn">
+                <div class="entry-image hidden-sm">
+                    <a href="#">
+                        <img src="images/gaby_fabro/novedades/congreso_la_plata.jpg" alt="Congreso La Plata">
+                        <div class="entry-date">Sep<span>13</span></div>
+                    </a>
+                </div>
+                <div class="entry-c">
+                    <div class="entry-title">
+                        <h2><a href="#">X Congreso de Danzas</a></h2>
+                    </div>
+                    <ul class="entry-meta clearfix">
+                        <li><a href="#"><i class="icon-time"></i> 13 de setiembre</a></li>
+                        <li><a href="#"><i class="icon-map-marker2"></i> Teatro El Bombín, Calle 59 N.886 entre 12 y 13, La Plata</a></li>
+                    </ul>
+                    <div class="entry-content">
+
+                    </div>
+                    <br><br>
+
+                    <a href="images/gaby_fabro/novedades/congreso_la_plata.jpg" class="button button-3d button-large button-rounded button-black"  data-lightbox="image"><i class="icon-camera"></i>Mas Información</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br>
     <div class="row" data-animate="fadeInRight">
         <div id="posts" class="events small-thumbs">
             <div class="entry clearfix" data-animate="fadeIn">
                 <div class="entry-image hidden-sm">
                     <a href="#">
-                        <img src="images/gaby_fabro/12208872_916939231708780_8012889015136329521_n.jpg" alt="Ominira">
-                        <!--<div class="entry-date">Sep<span>18</span></div>-->
+                        <img src="images/gaby_fabro/novedades/audicion.jpg" alt="Ominira">
+                        <div class="entry-date">Sep<span>15</span></div>
                     </a>
                 </div>
                 <div class="entry-c">
@@ -38,35 +67,6 @@
                     </div>
                     <br><br>
                     <a href="https://www.youtube.com/watch?v=vYnq8lYCFo8" class="button button-3d button-large button-rounded button-black"  data-lightbox="iframe"><i class="icon-video"></i>Trailer Ominira</a>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-    <div class="row" id="next-novedad" data-animate="fadeInRight">
-        <div id="posts" class="events small-thumbs">
-            <div class="entry clearfix" data-animate="fadeIn">
-                <div class="entry-image hidden-sm">
-                    <a href="#">
-                        <img src="images/gaby_fabro/novedades/congreso_la_plata.jpg" alt="Ominira">
-                        <div class="entry-date">Sep<span>13</span></div>
-                    </a>
-                </div>
-                <div class="entry-c">
-                    <div class="entry-title">
-                        <h2><a href="#">X Congreso de Danzas</a></h2>
-                    </div>
-                    <ul class="entry-meta clearfix">
-                        <li><a href="#"><i class="icon-time"></i> 13 de setiembre</a></li>
-                        <li><a href="#"><i class="icon-map-marker2"></i> Teatro El Bombín, Calle 59 N.886 entre 12 y 13, La Plata</a></li>
-                    </ul>
-                    <div class="entry-content">
-
-                    </div>
-                    <br><br>
-
-                    <a href="images/gaby_fabro/novedades/congreso_la_plata.jpg" class="button button-3d button-large button-rounded button-black"  data-lightbox="image"><i class="icon-camera"></i>Mas Información</a>
                 </div>
             </div>
         </div>
